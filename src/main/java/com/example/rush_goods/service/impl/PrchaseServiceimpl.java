82c8a50a725077f64f029e84a9e46b75063e839c
guohao <37768861+ScDadaguo@users.noreply.github.com>
2018-12-29 20:07:18 +0800

    使用时间戳限制重入的乐观锁实现抢购商品

--- src/main/java/com/example/rush_goods/service/impl/PrchaseServiceimpl.java
+++ src/main/java/com/example/rush_goods/service/impl/PrchaseServiceimpl.java
@@ -7,6 +7,8 @@ import com.example.rush_goods.pojo.PurchaseRecordPo;
 import com.example.rush_goods.service.PurchaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class PrchaseServiceimpl implements PurchaseService {
@@ -20,28 +22,42 @@ public class PrchaseServiceimpl implements PurchaseService {
 
 
     @Override
+    @Transactional(isolation = Isolation.READ_COMMITTED)
     public boolean purchase(Long userId, Long productId, int quantity) {
-//        获取产品
-        ProductPo product=productDao.getProduct(productId);
+        //      当前时间
+        long start = System.currentTimeMillis();
+//        循环尝试直至成功
+        while (true) {
+            long end = System.currentTimeMillis();
+            //如果循环时间大于100ms返回终止循环
+            if (end - start > 100) {
+                return false;
+            }
+
+
+            //        获取产品
+            ProductPo product = productDao.getProduct(productId);
 //        比较库存和购买数量
-        if (product.getStock()<quantity){
+            if (product.getStock() < quantity) {
 //            库存不足
-            return false;
-        }
+                return false;
+            }
 //      获取当前版本号
-        int version =product.getVersion();
+            int version = product.getVersion();
 //        扣减库存，同时将当前版本号发送给前台进行比较
-        int result=productDao.decreaseProduct(productId,quantity,version);
+            int result = productDao.decreaseProduct(productId, quantity, version);
 //        如果更新数据失败，说明数据在多线程中被其他线程，导致失败返回
-        if (result==0){
-            return false;
-        }
+//        当使用时间戳的时候，就然他继续循环，直至时间结束
+            if (result == 0) {
+                continue;
+            }
 
 //        初始化购买记录
-        PurchaseRecordPo pr=this.initPurchaseRecord(userId,product,quantity);
+            PurchaseRecordPo pr = this.initPurchaseRecord(userId, product, quantity);
 //        插入购买信息
-        purchaseRecordDao.insertPurchaseRecord(pr);
-        return true;
+            purchaseRecordDao.insertPurchaseRecord(pr);
+            return true;
+        }
     }
 
     /**
